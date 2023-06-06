fe189fdc309ddc90c6959532ba69d2ee2ffb5ae8
Andrea Damiani <andrea.dam90@gmail.com>
2023-06-06 11:33:41 +0200

    Corretto percorso immagine qrcode

--- src/components/ui/TheHeader.vue
+++ src/components/ui/TheHeader.vue
@@ -4,6 +4,6 @@
             <h1 class="text-5xl">Andrea Damiani</h1>
             <h2 class="text-2xl">Full Stack Web Developer Junior</h2>
         </div>
-        <img src="../../../assets/frame.png" class="w-24" alt="" />
+        <img src="/assets/frame.png" class="w-24" alt="" />
     </header>
 </template>
